085b60630f5d991b21e0e15e1e7156350a8c500d
karolmulato <161468438+karolmulato@users.noreply.github.com>
2024-06-04 11:09:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixapergunta = document.querySelector(".caixa-pergunta");
+const caixaaltenativa = document.querySelector(".caixa-altenativa");
+const caixaresultado = document.querySelector(".caixa-resultado");
+const caixaresultado = document.querySelector(".texto-resultado");
